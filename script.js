e5c3a5d8f827db3868bb352fb64f838a902420e8
ashutoshrai6395 <141047036+ashutoshrai6395@users.noreply.github.com>
2024-07-15 00:31:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var istatus=document.querySelector("h5")
+
+var addFriend=document.querySelector("#add")
+
+var removeFriend=document.querySelector("#remove")
+
+
+addFriend.addEventListener("click",function(){
+    istatus.innerHTML="Friends"
+    istatus.style.color="green"
+})
+
+removeFriend.addEventListener("click",function(){
+    istatus.innerHTML="Unknown"
+    istatus.style.color="red"
+})
